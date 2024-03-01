7df5ccdf8039859d55d5f62cf8e119102ccf0f99
kuldipsinh22 <131747159+kuldipsinh22@users.noreply.github.com>
2024-03-01 22:12:33 +0530

    design done

--- ssccserrors/src/App.js
+++ ssccserrors/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Blog from "./Components/Blog";
+import Projects from "./Components/Projects";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import Edit_profile from "./Components/Edit_profile";
+import Post from "./Components/Post";
+import Sidebar from "./Components/Sidebar";
+
+const Layout = () => {
+  return (
+    <>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Sidebar></Sidebar>
+    </>
+  );
+};
 
 function App() {
+  // const [auth, setAuth] = useState(sessionStorage.getItem("user"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <>
+          <Layout />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Services" element={<Services />} />
+            <Route path="/Blog" element={<Blog />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Edit_profile" element={<Edit_profile />} />
+            <Route path="/Post" element={<Post />} />
+          </Routes>
+          <Footer />
+        </>
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
